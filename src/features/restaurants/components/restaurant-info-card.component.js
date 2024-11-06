17ea53339cce059ebd8a183f98148e708ef917a4
Emily Knott <emknotty@gmail.com>
2024-11-06 22:25:05 +0800

    Add image files for star icon and restaurant icon, set up spacer component, update dependencies, optimize styled components in restaurant info card

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import { Text, Image } from 'react-native';
 import { Card } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+import { Spacer } from '../../../components/spacer/spacer.component';
+import star from '../../../../assets/star';
+import openIcon from '../../../../assets/open';
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -14,15 +19,40 @@ const RestaurantCardCover = styled(Card.Cover)`
 `;
 
 const Title = styled.Text`
-  padding: ${(props) => props.theme.space[3]};
   color: ${(props) => props.theme.colors.ui.primary};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  font-family: ${(props) => props.theme.fonts.heading};
+`;
+
+const Address = styled.Text`
   font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const RowRight = styled.View`
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = 'Some Restaurant',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
       'https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg',
       'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
@@ -30,8 +60,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     address = '100 Main Street',
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5}>
@@ -41,7 +73,51 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
           key={name}
         />
       </Card.Content>
-      <Title>{name}</Title>
+      <Info>
+        <Row>
+          <Title>{name}</Title>
+          <Image
+            style={{ width: 15, height: 15 }}
+            source={{ uri: icon }}
+          />
+        </Row>
+        <Row>
+          <Rating>
+            {ratingArray.map(() => {
+              return (
+                <SvgXml
+                  xml={star}
+                  width={20}
+                  height={20}
+                />
+              );
+            })}
+          </Rating>
+          <RowRight>
+            {isClosedTemporarily && (
+              <Text
+                variant='label'
+                style={{ color: 'red' }}
+              >
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer
+              position='left'
+              size='medium'
+            >
+              {isOpenNow && (
+                <SvgXml
+                  xml={openIcon}
+                  width={20}
+                  height={20}
+                />
+              )}
+            </Spacer>
+          </RowRight>
+        </Row>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
