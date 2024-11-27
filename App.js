67ac0e4d1cde6de03e8cffca128b7433cc8b48d4
Emily Knott <emknotty@gmail.com>
2024-11-27 21:06:16 +0800

    Set up location context for mock geocoding API call

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeArea } from './src/components/utility/safe-area.component';
 import { Ionicons } from '@expo/vector-icons';
-
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
+import { LocationContextProvider } from './src/services/location/location.context';
 
 const Tab = createBottomTabNavigator();
 
@@ -49,49 +49,51 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsContextProvider>
-          <NavigationContainer>
-            <Tab.Navigator
-              screenOptions={({ route }) => ({
-                tabBarIcon: ({ color, size }) => {
-                  let iconName;
+        <LocationContextProvider>
+          <RestaurantsContextProvider>
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ color, size }) => {
+                    let iconName;
 
-                  if (route.name === 'Restaurants') {
-                    iconName = 'restaurant';
-                  } else if (route.name === 'Settings') {
-                    iconName = 'settings-sharp';
-                  } else if (route.name === 'Map') {
-                    iconName = 'map';
-                  }
+                    if (route.name === 'Restaurants') {
+                      iconName = 'restaurant';
+                    } else if (route.name === 'Settings') {
+                      iconName = 'settings-sharp';
+                    } else if (route.name === 'Map') {
+                      iconName = 'map';
+                    }
 
-                  // You can return any component that you like here!
-                  return (
-                    <Ionicons
-                      name={iconName}
-                      size={size}
-                      color={color}
-                    />
-                  );
-                },
-                tabBarActiveTintColor: 'tomato',
-                tabBarInactiveTintColor: 'gray',
-              })}
-            >
-              <Tab.Screen
-                name='Restaurants'
-                component={RestaurantsScreen}
-              />
-              <Tab.Screen
-                name='Map'
-                component={Map}
-              />
-              <Tab.Screen
-                name='Settings'
-                component={Settings}
-              />
-            </Tab.Navigator>
-          </NavigationContainer>
-        </RestaurantsContextProvider>
+                    // You can return any component that you like here!
+                    return (
+                      <Ionicons
+                        name={iconName}
+                        size={size}
+                        color={color}
+                      />
+                    );
+                  },
+                  tabBarActiveTintColor: 'tomato',
+                  tabBarInactiveTintColor: 'gray',
+                })}
+              >
+                <Tab.Screen
+                  name='Restaurants'
+                  component={RestaurantsScreen}
+                />
+                <Tab.Screen
+                  name='Map'
+                  component={Map}
+                />
+                <Tab.Screen
+                  name='Settings'
+                  component={Settings}
+                />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </RestaurantsContextProvider>
+        </LocationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
---
+++ src/services/location/location.context.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, createContext } from 'react';
+import { locationRequest, locationTransform } from './location.service';
+
+export const LocationContext = createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [location, setLocation] = useState(null);
+  const [keyword, setKeyword] = useState('san francisco');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const onSearch = (searchKeyword = 'Antwerp') => {
+    console.log(searchKeyword.toLowerCase());
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+    locationRequest(searchKeyword)
+      .then(locationTransform)
+      .then((result) => {
+        setIsLoading(false);
+        setLocation(result);
+        console.log(result);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err);
+      });
+  };
+
+  useEffect(() => {
+    onSearch(keyword);
+  }, []);
+
+  return (
+    <LocationContext.Provider
+      value={{
+        isLoading,
+        error,
+        location,
+        search: onSearch,
+        keyword,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
