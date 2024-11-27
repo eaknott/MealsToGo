8e05e55d61a4b10b1cf961193b0476f3534017d1
Emily Knott <emknotty@gmail.com>
2024-11-27 20:05:05 +0800

    Add updated image links for restaurant images in mockImages array

--- src/services/restaurants/mock/index.js
+++ src/services/restaurants/mock/index.js
@@ -1,21 +1,34 @@
-import antwerp from "./antwerp.json";
-import chicago from "./chicago.json";
-import toronto from "./toronto.json";
-import san_francisco from "./san_francisco.json";
+import antwerp from './antwerp.json';
+import chicago from './chicago.json';
+import toronto from './toronto.json';
+import san_francisco from './san_francisco.json';
 
 export const mocks = {
-  "51.219448,4.402464": antwerp,
-  "43.653225,-79.383186": toronto,
-  "41.878113,-87.629799": chicago,
-  "37.7749295,-122.4194155": san_francisco,
+  '51.219448,4.402464': antwerp,
+  '43.653225,-79.383186': toronto,
+  '41.878113,-87.629799': chicago,
+  '37.7749295,-122.4194155': san_francisco,
 };
 
 export const mockImages = [
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-oranges-ice-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-baking-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-pancakes-600x750.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/messy-pizza-on-a-black-table-600x400.jpg",
-  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/pizza-ready-for-baking-600x400.jpg",
+  'https://craveablescafe.com/wp-content/uploads/2024/04/Pancake-Restaurant.jpg',
+  'https://www.bottleneckmgmt.com/wp-content/uploads/2023/08/header-burger-1071x675.jpg',
+  'https://nextrestaurants.com/wp-content/uploads/2019/10/Restaurant-Instagram-Photography.png',
+  'https://adamfirthphotography.com/wp-content/uploads/2017/05/A9O5409.jpg',
+  'https://images.squarespace-cdn.com/content/v1/58dd75f229687f5a850ec676/1635259297089-I5XO9QMKL0736RKKHRVL/TOP-5-RESTAURANT-FOOD-PHOTOGRAPHY-TIPS.jpg',
+  'https://lh6.googleusercontent.com/proxy/Ku4VM9tjmZIK1ZK2-EMX-MabKOpBVS0yy10jCQ9Ikft5o0fzeMiR2-K1Jme-M3k1j7EVwdc4cOu3VxxSL-wHy07LPoZ8BgUKDB336s8QiKtvFIW1wmyLif4pDjozkQdDgNCyJ3pi5ndiYSt-6WcN5nU8AkHWXXETkSyytpR8c3ks3g',
+  'https://www.bottleneckmgmt.com/wp-content/uploads/2023/08/Shareable.Flatlay-scaled.jpg',
+  'https://images.squarespace-cdn.com/content/v1/5a06aac2cf81e04b144237a5/1560403510321-I8593IIR0VOZQE37246K/IMG_9975.jpg',
+  'https://www.whitetruffleconsultants.com/wp-content/uploads/2020/05/food-photography.jpg',
+  'https://images.squarespace-cdn.com/content/v1/58dd75f229687f5a850ec676/1635260350310-URIQJIBBOC88X6ZUL2HJ/TOP-5-RESTAURANT-FOOD-PHOTOGRAPHY-TIPS.jpg',
+  'https://images.squarespace-cdn.com/content/v1/5867da18e4fcb5e16885642c/1553124344974-4Z5X6Q8I7ZAVRVB98GKF/AJP_6993.jpg',
+  'https://images.squarespace-cdn.com/content/v1/5c797c3b5239581bca07268a/90e1be8e-733c-4a33-a56c-171bba440736/DSC_3043.jpg',
+
+  // "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+  // "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-oranges-ice-600x750.jpg",
+  // "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  // "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-baking-600x750.jpg",
+  // "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-pancakes-600x750.jpg",
+  // "https://www.foodiesfeed.com/wp-content/uploads/2019/02/messy-pizza-on-a-black-table-600x400.jpg",
+  // "https://www.foodiesfeed.com/wp-content/uploads/2019/02/pizza-ready-for-baking-600x400.jpg",
 ];
