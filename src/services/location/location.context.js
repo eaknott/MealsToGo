4844244814f7ee5c742007aa0b953b595f59c919
Emily Knott <emknotty@gmail.com>
2024-11-28 14:01:59 +0800

    Pass in lowercased searchKeyword to locationRequest, clean up any console logs

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -4,8 +4,8 @@ import { locationRequest, locationTransform } from './location.service';
 export const LocationContext = createContext();
 
 export const LocationContextProvider = ({ children }) => {
-  const [location, setLocation] = useState(null);
   const [keyword, setKeyword] = useState('san francisco');
+  const [location, setLocation] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -16,7 +16,7 @@ export const LocationContextProvider = ({ children }) => {
       // don't do anything
       return;
     }
-    locationRequest(searchKeyword)
+    locationRequest(searchKeyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
         setIsLoading(false);
