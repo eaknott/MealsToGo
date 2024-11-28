336547993305f96f529ed268ee1b80b6e565c069
Emily Knott <emknotty@gmail.com>
2024-11-28 18:14:27 +0800

    Move navigation to separate components in infrastructure

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import {
@@ -7,33 +6,10 @@ import {
   Oswald_400Regular,
 } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
-import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 import { theme } from './src/infrastructure/theme';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SafeArea } from './src/components/utility/safe-area.component';
-import { Ionicons } from '@expo/vector-icons';
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 import { LocationContextProvider } from './src/services/location/location.context';
-
-const Tab = createBottomTabNavigator();
-
-const TAB_ICON = {
-  Restaurants: 'restaurant',
-  Map: 'map',
-  Settings: 'settings-sharp',
-};
-
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
-  </SafeArea>
-);
+import { Navigation } from './src/infrastructure/navigation/index';
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -51,47 +27,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator
-                screenOptions={({ route }) => ({
-                  tabBarIcon: ({ color, size }) => {
-                    let iconName;
-
-                    if (route.name === 'Restaurants') {
-                      iconName = 'restaurant';
-                    } else if (route.name === 'Settings') {
-                      iconName = 'settings-sharp';
-                    } else if (route.name === 'Map') {
-                      iconName = 'map';
-                    }
-
-                    // You can return any component that you like here!
-                    return (
-                      <Ionicons
-                        name={iconName}
-                        size={size}
-                        color={color}
-                      />
-                    );
-                  },
-                  tabBarActiveTintColor: 'tomato',
-                  tabBarInactiveTintColor: 'gray',
-                })}
-              >
-                <Tab.Screen
-                  name='Restaurants'
-                  component={RestaurantsScreen}
-                />
-                <Tab.Screen
-                  name='Map'
-                  component={Map}
-                />
-                <Tab.Screen
-                  name='Settings'
-                  component={Settings}
-                />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
